4aa202d9d25cc3e3ae0e6034efcef0a9ec289613
yumaito <yuma19950228@icloud.com>
2017-05-31 15:43:23 +0900

    キューブの接触時に効果音をつける GoForwardUnityChanをもとに作成 キューブが地面に接触する時とキューブ同士が積み重なるときに効果音をつける ユニティちゃんと衝突した時には効果音が鳴らないようにする 効果音は、音声ファイル「block」を使用する

---
+++ Assets/Lesson7/block.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class block : MonoBehaviour {
+
+	public AudioClip impact;
+	AudioSource audio;
+
+	void Start(){
+		audio = GetComponent<AudioSource> ();	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.tag == "block" || other.gameObject.tag == "ground") {
+			audio.PlayOneShot (impact, 0.7f);
+		}
+	}
+}
